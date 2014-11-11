282a3e7e4556ed8000c67ff775a5dd94d5d7f7d4
Adam Merrifield <macagp@gmail.com>
2014-11-11 12:00:05 -0500

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,44 +4,102 @@
 # This script creates symlinks from the home directory to any desired dotfiles
 # in ~/.dotfiles
 ############################
-
 # dotfiles directory
-dir=~/.dotfiles
+dir="$HOME/.dotfiles"
 # old dotfiles backup directory
-olddir=~/.dotfiles_old
+olddir="$HOME/.dotfiles_old"
 # list of files/folders to symlink in homedir
-files=(".bashrc" ".vimrc.bundle.local" ".vimrc.local" ".zshrc")
+files=".bashrc
+.vimrc.bundle.local
+.vimrc.local
+.zshrc"
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "###################################"
+echo "Creating $olddir for backup of any existing dotfiles in $HOME"
+rm -rf $olddir
 mkdir -p $olddir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file $olddir/
-done
 
 # install oh-my-zsh
 cd
-curl -L http://install.ohmyz.sh | sh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "###################################"
+    echo "Installing oh-my-zsh"
+    curl -L http://install.ohmyz.sh | sh
+fi
 
-# install seydoggy zsh theme
-ln -s ~/.dotfiles/seydoggy.zsh-theme ~/.oh-my-zsh/themes/
+echo "###################################"
+echo "install seydoggy zsh theme"
+# if file exists and is not a symlink
+if [ -e "$HOME/.oh-my-zsh/themes/seydoggy.zsh-theme" ]; then
+    if [ ! -h "$HOME/.oh-my-zsh/themes/seydoggy.zsh-theme" ]; then
+        mv $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme $HOME/.dotfiles_old/
+    else # remove symlink
+        rm -rf $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme
+    fi
+fi
+
+ln -s $dir/seydoggy.zsh-theme $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme
+if [ -h $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme ]; then
+    echo "symlink to $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme created."
+else
+    echo "Could not create symlink to $HOME/.oh-my-zsh/themes/seydoggy.zsh-theme"
+fi
 
 # install spf13-vim
 cd
-curl http://j.mp/spf13-vim3 -L -o - | sh
+if [ ! -d "$HOME/.spf13-vim-3" ]; then
+    echo "###################################"
+    echo "Installing spf13-vim"
+    curl http://j.mp/spf13-vim3 -L -o - | sh
+fi
 
-# install watchforchanges vim script
-ln -s ~/.dotfiles/watchforchanges ~/.vim/bundle/
+echo "###################################"
+echo "install watchforchanges vim script"
+# if dir exists and is not a symlink
+if [ -d "$HOME/.vim/bundle/watchforchanges" ]; then
+    if [ ! -h "$HOME/.vim/bundle/watchforchanges" ]; then
+        echo "$HOME/.vim/bundle/watchforchanges is not a symlink... moving to $olddir"
+        mv $HOME/.vim/bundle/watchforchanges $HOME/.dotfiles_old/
+    else # remove symlink
+        echo "$HOME/.vim/bundle/watchforchanges is a symlink... removing."
+        rm -rf $HOME/.vim/bundle/watchforchanges
+    fi
+fi
+
+ln -s $dir/watchforchanges $HOME/.vim/bundle/watchforchanges
+if [ -h $HOME/.vim/bundle/watchforchanges ]; then
+    echo "symlink to $HOME/.vim/bundle/watchforchanges created."
+else
+    echo "Could not create symlink to $HOME/.vim/bundle/watchforchanges"
+fi
 
 # create symlinks 
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+    echo "###################################"
+    echo "Handling $file..."
+
+    # if file exists and is not a symlink
+    if [ -e "$HOME/$file" ]; then
+        echo "Moving $file from $HOME to $olddir"
+
+        if [ ! -h "$HOME/$file" ]; then
+            echo "$file is not a symlink... moving to $olddir"
+            mv $HOME/$file $olddir/
+        else
+            echo "$file is a symlink... removing."
+            rm -rf $HOME/$file
+        fi
+    fi
+
+    echo "Creating symlink to $dir/$file in $HOME directory."
+    ln -s $dir/$file $HOME/$file
+    if [ -h $HOME/$file ]; then
+        echo "symlink to $HOME/$file created."
+    else
+        echo "Could not create symlink to $HOME/$file"
+    fi
 done
 
 # install bundles
-vim +BundleInstall! +BundleClean +q
+vim +BundleInstall! +BundleClean +qa!
