127a91736f5b6da35f94a23d5b0e983ba33b601a
Adam Merrifield <macagp@gmail.com>
2015-12-10 06:50:43 -0500

    Updated with debian commands

--- scripts/install-vim-with-lua.sh
+++ scripts/install-vim-with-lua.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    ruby-dev git-all
-
+# Debian
 sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common
+sudo apt-get install vim-gnome vim-nox
 
-cd ~
+# Something else
+#sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    #libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    #libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    #ruby-dev git-all
 
-git clone https://github.com/vim/vim.git
 
-cd vim
+#cd ~
 
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-perlinterp \
-            --enable-luainterp \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+#git clone https://github.com/vim/vim.git
 
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
+#cd vim
 
-sudo make install
+#./configure --with-features=huge \
+            #--enable-multibyte \
+            #--enable-rubyinterp \
+            #--enable-pythoninterp \
+            #--with-python-config-dir=/usr/lib/python2.7/config \
+            #--enable-perlinterp \
+            #--enable-luainterp \
+            #--enable-gui=gtk2 --enable-cscope --prefix=/usr
 
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-sudo update-alternatives --set editor /usr/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-sudo update-alternatives --set vi /usr/bin/vim
+#make VIMRUNTIMEDIR=/usr/share/vim/vim74
+
+#sudo make install
+
+#sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+#sudo update-alternatives --set editor /usr/bin/vim
+#sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+#sudo update-alternatives --set vi /usr/bin/vim
