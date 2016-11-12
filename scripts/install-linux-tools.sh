879d15840040090a628c1aa3fbb3e5d08ef15890
Adam Merrifield <macagp@gmail.com>
2016-11-11 22:53:48 -0500

    Removed old script files

--- scripts/install-linux-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Debian
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common
-sudo apt-get install vim-gnome vim-nox git zsh tmux xclip curl
-
--- scripts/install-tmux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install tmux
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
-cp tmuxifier_layouts/*.window.sh ../.tmuxifier/layouts/
